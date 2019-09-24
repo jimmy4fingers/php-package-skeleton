f9cfe19a7d847bd9daeeed1bef28873f60788c4f
jimmy4fingers <jimmy.lee.work@hotmail.co.uk>
2019-09-24 15:22:50 +0100

    updated default test suite to be unit tests.

---
+++ tests/MyTest.php
@@ -0,0 +1,19 @@
+<?php namespace ProjectNamespace\Tests;
+
+class MyTest extends \Codeception\Test\Unit
+{
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
